0529065a09764a95e00472fdf84086fc3d623159
Sasha Volokh <volokh@usc.edu>
2021-04-26 14:26:30 -0700

    reverted basic example

--- tests/basic/Main.java
+++ tests/basic/Main.java
@@ -3,15 +3,27 @@ import csci699cav.Concolic;
 public class Main {
     @Concolic.Entrypoint
     public static void h() {
-        StringBuilder sb = new StringBuilder();
-        sb.append('a');
-        sb.append('b');
-        sb.append(Concolic.inputInt());
-        String s = sb.toString();
-        Concolic.assertFalse(s.charAt(2) == 'x');
+        int x = Concolic.inputInt();
+        if (x < 0 || x > 4) {
+            return;
+        }
+
+        int sum = 0;
+        int last = 0;
+        for (int i = 0; i != x; ++i) {
+            int y = Concolic.inputInt();
+            if (y == last) {
+                return;
+            }
+            sum += y;
+            last = y;
+        }
+
+        Concolic.assertFalse(sum == 20);
     }
 
     public static void main(String[] args) {
         h();
     }
 }
+
